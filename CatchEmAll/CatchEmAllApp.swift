a957e9ee4391a6466889ef6eff64e496a4088e2b
GreenRamSilgen <kiranshrs@yahoo.com>
2026-01-23 08:31:34 -0800

    Initial Commit

---
+++ CatchEmAll/CatchEmAllApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatchEmAllApp.swift
+//  CatchEmAll
+//
+//  Created by Kiran Shrestha on 1/23/26.
+//
+
+import SwiftUI
+
+@main
+struct CatchEmAllApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
